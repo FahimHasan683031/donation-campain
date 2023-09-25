5919dd29f3e58a28a855103602c8544b64b52a5e
FahimHasan683031 <fahimhasan683031@gmail.com>
2023-09-26 02:59:56 +0600

    Statistic page design and pei chart design and statistic page all functionality complite

---
+++ src/Components/Pichart.jsx
@@ -0,0 +1,60 @@
+import { PieChart, Pie, Cell } from 'recharts';
+import { getLsData } from '../Utility/Utitity';
+import { useEffect, useState } from 'react';
+
+
+const PiChart = () => {
+    const lsDonation = getLsData()
+    const [selectDonations, setSelectDonations] = useState(0)
+    const [remainingDonations, setRemainingDonations] =useState(12)
+    useEffect(()=>{
+        setSelectDonations(lsDonation?.length)
+        setRemainingDonations(12-lsDonation?.length)
+    },[lsDonation])
+   
+
+    const data = [
+        { name: 'Group A', value: selectDonations },
+        { name: 'Group B', value: remainingDonations},
+
+    ];
+    const COLORS = ['#ff444a', '#00C49F'];
+
+    const RADIAN = Math.PI / 180;
+    const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent, index }) => {
+        const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+        const x = cx + radius * Math.cos(-midAngle * RADIAN);
+        const y = cy + radius * Math.sin(-midAngle * RADIAN);
+
+        return (
+            <text x={x} y={y} fill="white" textAnchor={x > cx ? 'start' : 'end'} dominantBaseline="central">
+                {`${(percent * 100).toFixed(0)}%`}
+            </text>
+        );
+    };
+
+
+    return (
+        <div>
+                <PieChart width={400} height={400}>
+                    <Pie
+                        data={data}
+                        cx="50%"
+                        cy="50%"
+                        labelLine={false}
+                        label={renderCustomizedLabel}
+                        outerRadius={150}
+                        fill="#8884d8"
+                        dataKey="value"
+                        className='font-bold text-xl'
+                    >
+                        {data.map((entry, index) => (
+                            <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+                        ))}
+                    </Pie>
+                </PieChart>
+        </div>
+    );
+};
+
+export default PiChart;
--- src/Pages/Statistics.jsx
+++ src/Pages/Statistics.jsx
@@ -1,10 +1,26 @@
+import PiChart from "../Components/Pichart";
 
 const Statistics = () => {
     return (
-        <div>
-            This is statistic page
+        <div className="text-center">
+            <div className="flex justify-center">
+                <PiChart></PiChart>
+            </div>
+            <div className="flex gap-7 md:gap-10 justify-center my-10">
+                <div className="flex gap-2 md:gap-4 items-center">
+                    <p className="text-[12px] md:text-base font-normal m:font-medium">Your Donation</p>
+                    <span className="w-12 md:w-20 h-[6px] md:h-[9px] rounded-sm bg-[#00c49f]"></span>
+                </div>
+                <div className="flex gap-2 md:gap-4 items-center">
+                    <p className="text-[12px] md:text-base font-normal m:font-medium">Total Donation</p>
+                    <span className="w-12 md:w-20  h-[6px] md:h-[9px] rounded-sm bg-[#ff444a]"></span>
+                </div>
+            </div>
         </div>
     );
 };
 
 export default Statistics;
+
+
+
